dd81c850659a0b2842b2c982f01f2815a37bf199
msanders-lg <msanders@lightmaregaming.net>
2014-11-15 22:32:06 -0500

    goto added

--- ALX/src/main/java/io/github/altaris/alx/ALXmain.java
+++ ALX/src/main/java/io/github/altaris/alx/ALXmain.java
@@ -1,8 +1,10 @@
 package io.github.altaris.alx;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ALXmain extends JavaPlugin {
@@ -22,8 +24,28 @@ public class ALXmain extends JavaPlugin {
 			
 			Player player = (Player) sender;
 			
-			return true;
+			//Arg 0 = first word entered after command, Arg 1 = second word, etc.
+			int length = args.length;
 			
+			if (length == 2) {
+				if (args[0].equalsIgnoreCase("goto")) {
+					
+					for (Player playerToGoto : Bukkit.getServer().getOnlinePlayers()) {
+						if(playerToGoto.getName().equalsIgnoreCase(args[1])) {
+							player.teleport(playerToGoto);
+							return true;
+						}
+						
+						return false;
+					}
+				}
+				
+				
+			}
+			
+			PluginDescriptionFile pdfFile = this.getDescription();
+			player.sendMessage(pdfFile.getName() + "Version" + pdfFile.getVersion() + ". Use !help for help.");
+			return true;
 		}
 		
 		return false;
--- ALX/src/main/java/io/github/altaris/alx/PlayerListener.java
+++ ALX/src/main/java/io/github/altaris/alx/PlayerListener.java
@@ -1,11 +1,30 @@
 package io.github.altaris.alx;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerListener implements Listener {
 	
+	List<String> replacements;
+	
+	public PlayerListener() {
+		super();
+		this.replacements = new ArrayList<String>();
+	}
+
 	public PlayerListener(ALXmain plugin) {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		String message = event.getMessage();
 		
 	}
 	
